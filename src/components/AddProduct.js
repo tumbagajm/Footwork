c67021526e7ffd0864dcf5999024b218926b3b92
tumbagajm <tumbagajohn@gmail.com>
2024-02-08 20:15:18 +0800

    fixed addProduct problem -- need more edit

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -8,7 +8,6 @@ const AddProduct = () => {
     const navigate = useNavigate();
     const { user } = useContext(UserContext);
 
-    console.log(user);
 
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
@@ -23,69 +22,58 @@ const AddProduct = () => {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem(token)}`,
+                Authorization: `Bearer ${token}`,
             },
-                body: JSON.stringify({
+            body: JSON.stringify({
                 name: name,
                 description: description,
                 price: price,
             }),
         })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
 
-            if (data.error === "Product Already exists.") {
-                Swal.fire({
-                    icon: "error",
-                    title: "Unsuccessful Product Creation",
-                    text: data.error,
-                });
-            } else if (data.message === "Product saved successfully!") {
-                Swal.fire({
-                    icon: "success",
-                    title: "Product Added",
-                    text: data.message,
-                });
+                if (data.message) {
+                    Swal.fire({
+                        icon: "success",
+                        title: "Product Added",
+                        text: data.message,
+                    });
+                } else {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Unsuccessful Product Creation",
+                        text: data.error,
+                    });
+                }
 
-                setName("");
-                setDescription("");
-                setPrice(0);
-            } else {
-                Swal.fire({
-                    title: "Something went wrong!",
-                    icon: "error",
-                    text: "There is a problem while adding the product.",
-                });
-
-                navigate("/products");
-            }
-        });
+            });
     };
-    return(
-		user.isAdmin === true ?
-		<>
-			<h1 className="my-5 text-center">Add Course</h1>
-			<Form onSubmit={e => createProduct(e)}>
-			    <Form.Group>
-			        <Form.Label>Name:</Form.Label>
-			        <Form.Control type="text" placeholder="Enter Name" required value={name} onChange={e => {setName(e.target.value)}}/>
-			    </Form.Group>
-			    <Form.Group>
-			        <Form.Label>Description:</Form.Label>
-			        <Form.Control type="text" placeholder="Enter Description" required value={description} onChange={e => {setDescription(e.target.value)}}/>
-			    </Form.Group>
-			    <Form.Group>
-			        <Form.Label>Price:</Form.Label>
-			        <Form.Control type="number" placeholder="Enter Price" required value={price} onChange={e => {setPrice(e.target.value)}}/>
-			    </Form.Group>
-			    <Button variant="primary" type="submit" className="my-5">Submit</Button>
-			</Form>
-		</>
-		:
-		<Navigate to="/*"/>
+    return (
+        user.isAdmin === true ?
+            <>
+                <h1 className="my-5 text-center">Add Course</h1>
+                <Form onSubmit={e => createProduct(e)}>
+                    <Form.Group>
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Name" required value={name} onChange={e => { setName(e.target.value) }} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Description:</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Description" required value={description} onChange={e => { setDescription(e.target.value) }} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Price:</Form.Label>
+                        <Form.Control type="number" placeholder="Enter Price" required value={price} onChange={e => { setPrice(e.target.value) }} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="my-5">Submit</Button>
+                </Form>
+            </>
+            :
+            <Navigate to="/*" />
 
-	);
+    );
 }
 
 export default AddProduct;
