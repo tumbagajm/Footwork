b40d79decefafa8094ca9ce9e38dd829883b4668
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-02-09 10:27:41 +0800

    Added ProductList to ProductSection and UserView. -Edited ProductCard style

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
-import ProductCard from "./products/ProductCard";
 import ProductList from "./products/ProductList";
 
 export default function UserView({ productsData }) {
