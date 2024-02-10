c0261fb71fc5371f35bb81b0a7287163cedd7d30
tumbagajohn <tumbagajohn@gmail.com>
2024-02-10 15:52:37 +0800

    Minor edit in appnavbar

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -54,7 +54,7 @@ const AppNavBar = () => {
                                     <div className="d-flex">
                                         <Nav.Link as={NavLink} to="/cart"><FontAwesomeIcon icon="fa-solid fa-cart-shopping" /></Nav.Link>
                                         <NavDropdown title={<FontAwesomeIcon icon="fa-solid fa-user" />} id="basic-nav-dropdown">
-                                            <NavDropdown.Item as={Link} to="/Profile">
+                                            <NavDropdown.Item as={Link} to="/profile">
                                                 Profile
                                             </NavDropdown.Item>
 
