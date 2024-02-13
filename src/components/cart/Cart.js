adb1b40233576bc7ab249d3d932e4473e9d337d9
renzcarlo.glorioso <renzcarlo.glorioso@gmail.com>
2024-02-13 14:05:01 +0800

    Removed order summary from cart when there are no items

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -40,58 +40,61 @@ const Cart = () => {
 
     return (
         <>  
-            <Col lg={7}>
-                {cart.cartItems.length === 0 ?
-                <p>No products found. <i class="fa-regular fa-face-frown"></i></p>
+            {
+                cart.cartItems.length === 0 ?
+                <p className="text-center">No products found. <i class="fa-regular fa-face-frown"></i></p>
                 :
-                <ul className="d-flex flex-column gap-5 list-unstyled">
-                    {cart.cartItems.map((item) => (
-                        <li key={item._id} className="d-flex p-2 gap-5 border-bottom">
-                            {item.productId.images.length > 0 ? (
-                                <img src={item.productId.images[0]} alt={item.productId.name} style={{ maxWidth: '100px' }} />
-                            ) : (
-                                <img src="https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg" alt="Default Image" style={{ maxWidth: '100px' }} />
-                            )}
+                <>
+                    <Col lg={7}>
+                        <ul className="d-flex flex-column gap-5 list-unstyled">
+                            {cart.cartItems.map((item) => (
+                                <li key={item._id} className="d-flex p-2 gap-5 border-bottom">
+                                    {item.productId.images.length > 0 ? (
+                                        <img src={item.productId.images[0]} alt={item.productId.name} style={{ maxWidth: '100px' }} />
+                                    ) : (
+                                        <img src="https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg" alt="Default Image" style={{ maxWidth: '100px' }} />
+                                    )}
+                                    <div>
+                                        <p>Product Name:{item.productId.name}</p>
+                                        <p>Quantity: {item.quantity}</p>
+                                        <p>Subtotal: &#8369;{item.subtotal}</p>
+                                    </div>
+                                    <div className="ms-auto">
+                                        <RemoveFromCart cartId={item.productId._id}/>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </Col>
+                    <Col lg={5}>
+                        <div className="d-flex flex-column gap-3 p-3 p-md-5 bg-light border rounded-4 sticky-top">
+                            <h2>Order Summary</h2>
+                            <div className="p-1 border-bottom"> 
+                                <div className="d-flex justify-content-between">
+                                    <p>Subtotal</p>
+                                    <p className="fw-bold">&#8369;{cart.totalPrice}</p>
+                                </div>
+                                <div className="d-flex justify-content-between">
+                                    <p>Shipping estimate</p>
+                                    <p className="fw-bold">&#8369;0.00</p>
+                                </div>
+                                <div className="d-flex justify-content-between">
+                                    <p>Tax estimate</p>
+                                    <p className="fw-bold">&#8369;0.00</p>
+                                </div>
+                            </div>
                             <div>
-                                <p>Product Name:{item.productId.name}</p>
-                                <p>Quantity: {item.quantity}</p>
-                                <p>Subtotal: &#8369;{item.subtotal}</p>
+                                <div className="d-flex justify-content-between">
+                                    <h2 className="fw-bold">Total</h2>
+                                    <h2 className="fw-bold">&#8369;{cart.totalPrice}</h2>
+                                </div>
                             </div>
-                            <div className="ms-auto">
-                                <RemoveFromCart cartId={item.productId._id}/>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-                }
-            </Col>
-            <Col lg={5}>
-                <div className="d-flex flex-column gap-3 p-3 p-md-5 bg-light border rounded-4 sticky-top">
-                    <h2>Order Summary</h2>
-                    <div className="p-1 border-bottom"> 
-                        <div className="d-flex justify-content-between">
-                            <p>Subtotal</p>
-                            <p className="fw-bold">&#8369;{cart.totalPrice}</p>
+                            <Checkout />
+                            <Link to="/products" type="button" className="p-3 btn btn-outline-dark">Continue Shopping</Link>
                         </div>
-                        <div className="d-flex justify-content-between">
-                            <p>Shipping estimate</p>
-                            <p className="fw-bold">&#8369;0.00</p>
-                        </div>
-                        <div className="d-flex justify-content-between">
-                            <p>Tax estimate</p>
-                            <p className="fw-bold">&#8369;0.00</p>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="d-flex justify-content-between">
-                            <h2 className="fw-bold">Total</h2>
-                            <h2 className="fw-bold">&#8369;{cart.totalPrice}</h2>
-                        </div>
-                    </div>
-                    <Checkout />
-                    <Link to="/products" type="button" className="p-3 btn btn-outline-dark">Continue Shopping</Link>
-                </div>
-            </Col>
+                    </Col>
+                </>
+            }
         </>
     );
 };
